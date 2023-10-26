7afe194c0c9a56114ed5aac1bd33c9df76f0edf5
Evilis Glenio <glenio.developer@gmail.com>
2023-10-26 09:24:45 -0300

    Any updates of Header Components, and any updates

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,15 @@
+import CartButton from "../CartButton";
 import Logo from "../Logo";
+import UserNav from "../UserNav";
 
 export default function Header() {
     return (<header className="sticky top-0 z-10 shadow">
         <div className="container ms-auto p-4 flex items-center justify-between">
             <Logo/>
+            <div className="flex items-center justify-center space-x-4">
+                <CartButton />
+                <UserNav />
+            </div>
         </div>
     </header>)
 }

2e8381095e71fc03cab8a82c975dc0ea5e8f9dd6
Evilis Glenio <glenio.developer@gmail.com>
2023-10-26 12:19:36 -0300

    Products page

--- app/dummy/seed/page.tsx
+++ app/dummy/seed/page.tsx
@@ -1,6 +1,38 @@
 import stripe from "@/lib/stripe"
+import { DummyProduct } from "@/types"
 
-export default function Seed(){
+async function getDummyProducts(){
+    const response = await fetch("https://dummyjson.com/products?limit=9")
+    const dummyData = await response.json()
+    const products = dummyData.products.map((product: DummyProduct) => {
+        return {
+            id: product.id,
+            description: product.description,
+            name: product.title,
+            images: product.images,
+            default_price_data: {
+                unit_amount_decimal: product.price,
+                currency: "BRL",
+            }
+        }
+    })
+    return products
+}
+
+async function seedDummyData(){
+    const products = await getDummyProducts()
+    await products.map(async (product: any) => {
+        try {
+            const productCreated = await stripe.products.create(product)
+            console.log(productCreated.name)
+        } catch (error: any) {
+            console.log("STRIPE_CREATE_ERROR: ", error.message)
+        }
+    })
+}
+
+export default async function Seed(){
+    await seedDummyData()
     return(<div className="container flex items-center justify-center my-10">
         <h1 className="text-3xl text-green-600 font-extrabold">
             Dummy date created in your Stripe Inventory. If you don't see it on your Products Dashboard. Take a look at your console log.

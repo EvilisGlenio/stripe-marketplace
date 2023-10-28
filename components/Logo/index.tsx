478b0ad674704ca2e056e13a1a0e24377393c49c
Evilis Glenio <glenio.developer@gmail.com>
2023-10-28 11:49:46 -0300

    update to store name and store logo

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,9 +1,9 @@
-import { Store } from "lucide-react";
+import { MonitorSmartphone } from "lucide-react";
 import Link from "next/link";
 
 export default function Logo() {
     return (<Link href={"/"} className="flex items-center justify-center gap-2 text-xl font-extrabold text-sky-500">
-        <Store className="w-10 h-10 text-sky-600"/>
-        <span className=" hidden sm:inline-block">Live MKT</span>
+        <MonitorSmartphone className="w-10 h-10 text-sky-600"/>
+        <span className=" hidden sm:inline-block">Stripe Store</span>
     </Link>)
 }

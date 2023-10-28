d9c7dc1e671547bb987ff314fd895ad818ac2949
Evilis Glenio <glenio.developer@gmail.com>
2023-10-28 11:50:19 -0300

    update to bg header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import Logo from "../Logo";
 import UserNav from "../UserNav";
 
 export default function Header() {
-    return (<header className="sticky top-0 z-10 shadow">
+    return (<header className="sticky top-0 z-10 shadow bg-white">
         <div className="container ms-auto p-4 flex items-center justify-between">
             <Logo/>
             <div className="flex items-center justify-center space-x-4">

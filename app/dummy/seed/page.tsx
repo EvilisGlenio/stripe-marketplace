53c6d5fd1f410a495cb99f934475683740c66e78
Evilis Glenio <glenio.developer@gmail.com>
2023-10-28 12:21:01 -0300

    erros correction

--- app/dummy/seed/page.tsx
+++ app/dummy/seed/page.tsx
@@ -37,7 +37,7 @@ export default async function Seed(){
     await seedDummyData()
     return(<div className="container flex items-center justify-center my-10">
         <h1 className="text-3xl text-green-600 font-extrabold">
-            Dummy date created in your Stripe Inventory. If you don't see it on your Products Dashboard. Take a look at your console log.
+            Dummy date created in your Stripe Inventory. If you dont see it on your Products Dashboard. Take a look at your console log.
         </h1>
     </div>)
 }
